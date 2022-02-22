865d4a61ce46e3922106ecaa4d1c072b7670e0bb
kudrix <max51kud@gmail.com>
2022-02-22 19:31:49 +0500

    ёмаё, мама звонит

--- app/src/main/java/com/example/boaba/MainActivity.java
+++ app/src/main/java/com/example/boaba/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //C 23 февраля, брат. Скажи домашку по алгебере
     }
 }
